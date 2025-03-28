2047d6b347a839c739e213cae24cd60ef9c00927
HeddaRC <regiiconci@gmail.com>
2025-03-28 14:07:31 -0300

    Se subio el cambio!!

--- Program.cs
+++ Program.cs
@@ -5,5 +5,6 @@ class Program
     static void main()
     {
         Console.WriteLine("Lindo viernes para comer guiso de arroz y Visual Studio 2022!!!");
+        Console.WriteLine("Nico Paz de titular!!");
     }
 }
